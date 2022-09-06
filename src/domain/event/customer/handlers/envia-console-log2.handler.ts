6d26b014221ca76228047db276a4c97357849e83
Vinicius Severo <vinicius.severo@outlook.com>
2022-09-06 08:37:38 -0300

    Fix second handler message

--- src/domain/event/customer/handlers/envia-console-log2.handler.ts
+++ src/domain/event/customer/handlers/envia-console-log2.handler.ts
@@ -3,7 +3,7 @@ import CustomerAddressChangedEvent from "../customer-created.event";
 
 export default class EnviaConsoleLog2Handler implements EventHandlerInterface<CustomerAddressChangedEvent> {
     handle(event: CustomerAddressChangedEvent): void {
-        console.log(`Endereço do cliente:  ${event.eventData.id},  ${event.eventData.name} alterado para:  ${event.eventData.Address}`)
+        console.log(`Esse é o segundo console.log do evento: CustomerCreated.`)
     }
     
 }
